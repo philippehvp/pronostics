40338698e5fa474a7aa2d5e88835a6ba4003b158
philippehvp <philippe_hvp@hotmail.com>
2020-10-22 22:16:47 +0200

    Update commun_entete.php

--- commun_entete.php
+++ commun_entete.php
@@ -2,15 +2,21 @@
 <meta name="viewport" content="width=1400, initial-scale=1.0">
 
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<script type="text/javascript" src="js/jquery/jquery-1.10.2.js"></script>
+<!-- <script type="text/javascript" src="js/jquery/jquery-1.10.2.js"></script>
 <script type="text/javascript" src="js/jquery-ui/jquery-ui.custom.min.js"></script>
+<script src="js/jquery/jquery.mousewheel.js"></script>
+<script src="js/jquery/jquery.nicescroll.min.js"></script> -->
 
+<!-- <script type="text/javascript" src="js/jquery/jquery-3.5.1.min.js"></script> -->
+<script type="text/javascript" src="js/jquery/jquery-1.10.2.js"></script>
+<script type="text/javascript" src="js/jquery/jquery.mousewheel.js"></script>
+<script type="text/javascript" src="js/jquery/jquery.nicescroll.min.js"></script>
+<script type="text/javascript" src="js/jquery-ui/jquery-ui.custom.min.js"></script>
 
 <script type="text/javascript" src="js/soundmanager/soundmanager2-jsmin.js"></script>
 <script type="text/javascript" src="js/pronostics/pronostics.js"></script>
 
-<script src="js/jquery/jquery.mousewheel.js"></script>
-<script src="js/jquery/jquery.nicescroll.min.js"></script>
+<script type="text/javascript" src="js/cssemoticons/jquery.cssemoticons.min.js"></script>
 
 <link rel="stylesheet" href="css/custom-theme/jquery-ui-1.10.4.custom.css" />
 <link rel="stylesheet" href="css/datatables/jquery.dataTables.min.css" />
@@ -21,9 +27,9 @@
 <link rel="stylesheet" href="css/pronostics/themes/<?php echo (isset($_SESSION["theme_pronostiqueur"]) ? $_SESSION["theme_pronostiqueur"] : 'defaut');?>/tableaux.css" />
 <link rel="stylesheet" href="css/pronostics/themes/<?php echo (isset($_SESSION["theme_pronostiqueur"]) ? $_SESSION["theme_pronostiqueur"] : 'defaut');?>/style.css" />
 
-<link href="css/cssemoticons/jquery.cssemoticons.css" media="screen" rel="stylesheet" type="text/css" />
-<script src="js/cssemoticons/jquery.cssemoticons.min.js" type="text/javascript"></script>
-
+<link rel="stylesheet" href="css/cssemoticons/jquery.cssemoticons.css" media="screen" type="text/css" />
 <link rel="icon" type="image/png" href="images/poulpe.png" />
+
+
 
 <title>Le Poulpe d'Or</title>
