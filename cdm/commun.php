e2210bfe3c6a92f4befe790c966cf371df6cfd9e
philippehvp <philippe_hvp@hotmail.com>
2021-09-15 11:04:56 +0200

    Correction connexion

--- cdm/commun.php
+++ cdm/commun.php
@@ -18,7 +18,7 @@
 		if($_SERVER['HTTP_HOST'] == 'localhost') {
 			$bdd = new PDO('mysql:host=localhost;dbname=lepoulpeg', 'root', '', array(\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 		} else {
-			$bdd = new PDO('mysql:host=mysql51-119.perso;dbname=lepoulpeg', 'lepoulpeg', 'Allezlom2014', array(\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+			$bdd = new PDO('mysql:host=lepoulpeg.mysql.db;dbname=lepoulpeg', 'lepoulpeg', 'Allezlom2014', array(\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 		}
 	}
 	catch(Exception $e) {
--- poulpe/dist/common.php
+++ poulpe/dist/common.php
@@ -9,7 +9,7 @@
 			$db = new PDO('mysql:host=localhost;dbname=lepoulpeg', 'root', '', array(\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 		}
 		else {
-			$db = new PDO('mysql:host=mysql51-119.perso;dbname=lepoulpeg', 'lepoulpeg', 'Allezlom2014', array(\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+			$db = new PDO('mysql:host=lepoulpeg.mysql.db;dbname=lepoulpeg', 'lepoulpeg', 'Allezlom2014', array(\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 		}
 	}
 	catch(Exception $e) {
