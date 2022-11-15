34bab90e00976085c8f8426d428796f13de97262
Philippe Huynh Vinh Phat <Philippe@mbp-m1-de-philippe-1.home>
2022-11-15 22:21:53 +0100

    Ajout indentation

--- gerer_pronostiqueurs_creation_pronostiqueur.php
+++ gerer_pronostiqueurs_creation_pronostiqueur.php
@@ -9,7 +9,7 @@
 	$nomFamille = isset($_POST["nomFamille"]) ? $_POST["nomFamille"] : '';
 	$motDePasse = isset($_POST["motDePasse"]) ? $_POST["motDePasse"] : '';
 
-  $ordreSQL =   ' CALL    sp_creationpronostiqueur('. $bdd->quote($nomUtilisateur) . ', ' . $bdd->quote($nomFamille) . ', ' . $bdd->quote($prenom) . ', ' . $bdd->quote($motDePasse) . ', NULL, NULL, 3)';
+  	$ordreSQL =   ' CALL    sp_creationpronostiqueur('. $bdd->quote($nomUtilisateur) . ', ' . $bdd->quote($nomFamille) . ', ' . $bdd->quote($prenom) . ', ' . $bdd->quote($motDePasse) . ', NULL, NULL, 3)';
 
 	$bdd->exec($ordreSQL);
 
